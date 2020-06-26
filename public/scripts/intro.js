8afe9ee8580fc6f91fbf81c3dcff35ef7894382e
Ananda Steffens <ananda.steffens@hotmail.com>
2020-06-26 20:20:22 -0300

    first commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,59 @@
+// comments
+//document.write("Hello")
+
+//variaveis, tipo de dados
+//var myvar = "Hi"
+//const myconst = "He"
+//document.write(myvar + myconst)
+//string -> ""; ''; ``
+
+//number
+//const n1 = 1
+//const n2= 13
+//document.write(n1+n2)
+
+//boolean
+//const bTrue = true
+//const bFalse = false
+//document.write(true)
+
+//objeto possuem propriedades e funcionalidades
+//const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: true,
+//  correr(){
+//    return "run Forest"
+//  }
+//}
+//document.write(pessoa.correr())
+
+//Array - vetores - coleção de dados
+//const colecaoDeBolinhas = ["blue", "green", 1, {name: "name"}]
+//document.write(colecaoDeBolinhas[3].name)
+
+//Funções - Funcionalidades - Atalhos - Reuso de código
+
+//registrar função
+//function sayMyName(name){
+//  document.write(name)
+//}
+
+//executar
+//("Douglas")
+//sayMyName("Ana")
+
+
+//condicionais
+//const notaFinal = 7
+//if(notaFinal <5){
+//  document.write("Reprovado")
+//}else{
+//  document.write("Aprovado")
+//}
+
+// loop repetições
+
+//for (i = 0; i < 10; i++){
+  //  document.write(`<p>${i}</p>`)
+  //}
